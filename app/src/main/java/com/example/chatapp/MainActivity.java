340dc75e52edaf17b5045f99f7d0a43a3096e1cd
shaminda <shaminda.u.b.p@gmail.com>
2020-12-28 19:38:44 +0530

    added 3 tabs

--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -1,14 +1,77 @@
 package com.example.chatapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.example.chatapp.databinding.ActivityMainBinding;
+import com.example.chatapp.manu.ChatsFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+        setUpViewPager(binding.viewPage);
+        binding.tabLayout.setupWithViewPager(binding.viewPage);
+
+    }
+
+
+
+    private void setUpViewPager(ViewPager viewPager){
+
+        MainActivity.SectionPagerAdapter adapter = new SectionPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new ChatsFragment(),"Chats");
+        adapter.addFragment(new ChatsFragment(),"Status");
+        adapter.addFragment(new ChatsFragment(),"Friends");
+
+        viewPager.setAdapter(adapter);
+
+    }
+
+    private static class SectionPagerAdapter extends FragmentPagerAdapter{
+
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public SectionPagerAdapter(FragmentManager manager){
+            super(manager);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+        public void addFragment (Fragment fragment,String title){
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @NonNull
+        @Override
+        public CharSequence getPageTitle(int position){
+            return mFragmentTitleList.get(position);
+        }
+
     }
 }
